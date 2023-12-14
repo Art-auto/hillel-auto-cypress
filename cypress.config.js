6aab736004546abe4dcaec8c5da29f7a7361a12f
Artem Shoiko <shoiko.a.s@gmail.com>
2023-12-14 21:15:33 +0200

    Lesson 19 - BDD

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,21 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require("cypress")
+const cucumber = require("cypress-cucumber-preprocessor").default
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("file:preprocessor", cucumber());
+      
     },
+    specPattern: ['cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',"**/*.feature"],
+    baseUrl: 'https://qauto.forstudy.space'
   },
+  retries: {
+    // Configure retry attempts for `cypress run`
+    // Default is 0
+    runMode: 1,
+    // Configure retry attempts for `cypress open`
+    // Default is 0
+    openMode: 0
+  }
 });
